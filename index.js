a26c599c4cde594c9ca37a4e9bbf96842a7521c7
April Reth <april.reth@gmail.com>
2022-01-27 18:56:46 -0500

    Adding styles to index.js, text and images

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let pageHeader = document.querySelector("#page-header");
+pageHeader.style.backgroundColor = "black";
+pageHeader.style.color = "white";
 
-// Select some elements...
+let dogNames = document.querySelector(".dog-name")
+for(let i = 0; i < dogNames.length; i++) {
+    dogNames[i].style.color = "red";
+}
+
+let dogImages = document.querySelectorAll(".dog-images");
+for(let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = "15px";
+}
